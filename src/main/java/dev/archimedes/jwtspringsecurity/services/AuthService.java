8b54c707479fc1b5de8b13eefc4e3d7342d6269b
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-08-17 08:27:30 +0530

    Revert "Initial Commit"
    This reverts commit c042a9595d826b91c41cbe3074773c89403b595d.

--- src/main/java/dev/archimedes/jwtspringsecurity/services/AuthService.java
+++
@@ -1,8 +0,0 @@
-package dev.archimedes.jwtspringsecurity.services;
-
-import dev.archimedes.jwtspringsecurity.dtos.SignUpRequest;
-import dev.archimedes.jwtspringsecurity.dtos.UserDTO;
-
-public interface AuthService {
-    UserDTO createUser(SignUpRequest signUpRequest);
-}
